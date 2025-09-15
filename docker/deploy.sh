575e52abe054abb742a4502789c47a39f857507d
luis122448 <luis122448@gmail.com>
2025-09-14 23:58:29 -0500

    fix: mirror changes in docker

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -4,5 +4,8 @@
 # Stop the application
 sudo docker compose down
 
+# Pull the latest images
+sudo docker compose pull
+
 # Build and run the application
 sudo docker compose up --build --force-recreate --no-deps -d
